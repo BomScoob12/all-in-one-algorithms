55824052ed21694bd44c2b01a82497a0e2f1b24b
BomScoob12 <sorawitkraukham@gmail.com>
2024-11-01 00:11:10 +0700

    feat: bubble sort.

--- src/main/java/com/bomscoob/Main.java
+++ src/main/java/com/bomscoob/Main.java
@@ -1,7 +1,19 @@
 package com.bomscoob;
 
+import com.bomscoob.algorithms.sort.BubbleSort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        testBubbleSort();
+    }
+
+    private static void testBubbleSort() {
+        System.out.println("Bubble sort!!!");
+        BubbleSort bubbleSort = new BubbleSort();
+        Integer[] arr = new Integer[]{2,5,1,7,5,9};
+        String sorted = Arrays.toString(bubbleSort.sort(arr));
+        System.out.println(sorted);
     }
 }
--- src/main/java/com/bomscoob/algorithms/sort/BubbleSort.java
+++ src/main/java/com/bomscoob/algorithms/sort/BubbleSort.java
@@ -2,19 +2,22 @@ package com.bomscoob.algorithms.sort;
 
 import com.bomscoob.algorithms.interfaces.ISort;
 
+import java.util.Arrays;
+
 public class BubbleSort implements ISort<Integer> {
 
     @Override
     public Integer[] sort(Integer[] array) {
         for (int out = 0; out<array.length; out++) {
+            System.out.println(Arrays.toString(array));
             for (int in = 0; in<array.length; in++) {
-                if (array[in] > array[out]) {
-                    int temp = array[in];
-                    array[in] = array[out];
-                    array[out] = temp;
+                if (array[out] > array[in]) {
+                    int temp = array[out];
+                    array[out] = array[in];
+                    array[in] = temp;
                 }
             }
         }
-        return new Integer[0];
+        return array;
     }
 }
